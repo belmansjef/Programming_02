b414b7dfef6983c11d10dcd9f86a460d2e26035c
Jef Belmans <belmansjef@gmail.com>
2022-04-29 17:09:28 +0200

    Finished PrseSVGFile

--- 1DAE21_08_Belmans_Jef/W08/ParseSVGFile/ParseSVGFile.cpp
+++ 1DAE21_08_Belmans_Jef/W08/ParseSVGFile/ParseSVGFile.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <iomanip>
 #include "structs.h"
+
+const std::string WHITESPACE = " \n\r\t\f\v";
 
 std::string GetAttributeValue(const std::string& attrName, const std::string& element);
 Point2f ToPoint2f(const std::string& point2fStr);
+std::string ltrim(const std::string& s);
+void ProcessAttribute(const std::string& s);
+std::string PointToString(const std::string& s);
 
 int main()
 {
@@ -13,13 +19,29 @@ int main()
 
 	if (levelFile && triangleFile)
 	{
+		while (levelFile.peek() != EOF)
+		{
+			std::string line;
+			std::getline(levelFile, line);
+			line = ltrim(line);
+			std::string attribute{ GetAttributeValue("d", line) };
+			
+			if (line[0] == 'd')
+			{
+				ProcessAttribute(attribute);
+			}
+		}
+
 		while (triangleFile.peek() != EOF)
 		{
 			std::string line;
 			std::getline(triangleFile, line);
-			if (GetAttributeValue("d", line) != "")
+			line = ltrim(line);
+			std::string attribute{ GetAttributeValue("d", line) };
+
+			if (line[0] == 'd')
 			{
-				std::cout << "Found D attr!" << line << std::endl;
+				ProcessAttribute(attribute);
 			}
 		}
 	}
@@ -56,4 +78,51 @@ Point2f ToPoint2f(const std::string& point2fStr)
 	point.y = std::stof(attr);
 
 	return point;
+}
+
+// Trim all whitespaces at the beginning of the string
+std::string ltrim(const std::string& s)
+{
+	size_t start = s.find_first_not_of(WHITESPACE);
+	return (start == std::string::npos) ? " " : s.substr(start);
+}
+
+void ProcessAttribute(const std::string& s)
+{
+	std::stringstream ss{ s };
+
+	while (ss.peek() != EOF)
+	{
+		std::string attr;
+		std::getline(ss, attr, ' ');
+
+		if (!isdigit(attr[0]) && attr[0] != '-')
+		{
+			if (attr.size() > 1)
+			{
+				std::cout << std::endl << "Command: " << attr[0] << std::endl;
+				attr.erase(0, 1);
+
+				std::cout << PointToString(attr);
+			}
+			else
+			{
+				std::cout << std::endl << "Command: " << attr[0] << std::endl;
+			}
+		}
+		else
+		{
+			std::cout << PointToString(attr);
+		}
+	}
+}
+
+std::string PointToString(const std::string& s)
+{
+	std::stringstream pointss;
+	Point2f p{ ToPoint2f(s) };
+	pointss.precision(2);
+	pointss.setf(std::ios::fixed);
+	pointss << "[" << p.x << ", " << p.y << "]" << std::endl; 
+	return pointss.str();
 }
